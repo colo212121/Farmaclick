4c2f96119af39fa2273a96d0a8e58af08626bfcb
Android Studio <devandroid@ort>
2025-04-25 12:12:32 -0300

    cambiando el css

--- LoginFarmaclick/obj/Debug/net7.0/LoginFarmaclick.AssemblyInfo.cs
+++ LoginFarmaclick/obj/Debug/net7.0/LoginFarmaclick.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginFarmaclick")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2218c76900516cf19ed61f08596842c85656fbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ed5e8e96c692dbaf28a208edc206a54edad519d")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginFarmaclick")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginFarmaclick")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
